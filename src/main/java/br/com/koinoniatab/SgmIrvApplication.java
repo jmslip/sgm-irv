fc4f980906e3f3c06088cb1f753c068a43fe1f6a
Jorge Marcelo de Souza Junior <jms.slip@gmail.com>
2025-05-23 20:21:18 -0300

    First commit

---
+++ src/main/java/br/com/koinoniatab/SgmIrvApplication.java
@@ -0,0 +1,13 @@
+package br.com.koinoniatab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgmIrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgmIrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/koinoniatab/SgmIrvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.koinoniatab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgmIrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
